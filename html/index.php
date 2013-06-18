13cc2095064b4d0c07e49e2df43a9c48f78d7b8e
Omarito <omar_2412@live.com>
2013-06-18 15:48:35 -0400

    Get quotes done

--- html/index.php
+++ html/index.php
@@ -1,6 +1,7 @@
 <?php
-echo '"hello, World!"';
-
-
-
+session_start();
+include("../php/helpers.php");
+renderTop();
+echo '<p> Welcome to CS75 Finance, Your gate to the stock market, At CS75 Finance you will get real time quotes about stocks you desire </p>';
+renderBottom();
 ?>
